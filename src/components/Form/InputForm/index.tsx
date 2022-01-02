3a37d5e722c05a0f1494b97c53963ebfe1dd0e00
Domingos Capitango <p.capitango65@gmail.com>
2022-01-02 03:00:03 +0100

    :sparkles: add inputs validation.

--- src/components/Form/InputForm/index.tsx
+++ src/components/Form/InputForm/index.tsx
@@ -1,15 +1,16 @@
 import React from "react";
 import { TextInputProps } from "react-native";
 import { Control, Controller } from "react-hook-form";
-import { Container } from "./styles";
+import { Container, Error } from "./styles";
 import { Input } from "../Input";
 
 interface Props extends TextInputProps {
   control: Control;
   name: string;
+  error: string;
 }
 
-export const InputForm = ({ control, name, ...rest }: Props) => {
+export const InputForm = ({ control, name, error, ...rest }: Props) => {
   return (
     <Container>
       <Controller
@@ -19,6 +20,7 @@ export const InputForm = ({ control, name, ...rest }: Props) => {
           <Input onChangeText={onChange} value={value} {...rest} />
         )}
       />
+      {error && <Error>{error}</Error>}
     </Container>
   );
 };
--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
-import { Input } from "../../components/Form/Input";
+import { Keyboard, Modal, TouchableWithoutFeedback, Alert } from "react-native";
 import { Button } from "../../components/Form/Button";
-import { Modal } from "react-native";
 import { useForm } from "react-hook-form";
 
 import {
@@ -16,11 +15,21 @@ import { TransactionTypeButton } from "../../components/Form/TransactionTypeButt
 import { CategorySelectButton } from "../../components/Form/CategorySelectButton";
 import { CategorySelect } from "../CategorySelect";
 import { InputForm } from "../../components/Form/InputForm";
+import * as Yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
 
 interface FormData {
   name: string;
   amount: string;
 }
+
+const schema = Yup.object().shape({
+  name: Yup.string().required("Nome é obrigatório."),
+  amount: Yup.number()
+    .typeError("Informe um valor numérico!")
+    .positive("O valor não pode ser negativo.")
+    .required("O valor é obrigatório"),
+});
 
 export function Register() {
   const [category, setCategory] = useState({
@@ -30,7 +39,11 @@ export function Register() {
   const [transactionType, setTransactionType] = useState("");
   const [categoryModalOpen, setCategoryModalOpen] = useState(false);
 
-  const { control, handleSubmit } = useForm();
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ resolver: yupResolver(schema) });
 
   function handleTransactionTypeSelect(type: "up" | "down") {
     setTransactionType(type);
@@ -45,6 +58,13 @@ export function Register() {
   }
 
   function handleRegister(form: FormData) {
+    if (!transactionType) {
+      return Alert.alert("Selecione o tipo da transação.");
+    }
+
+    if (category.key === "category") {
+      return Alert.alert("Selecione a categoria.");
+    }
     const { name, amount } = form;
     const data = {
       name,
@@ -56,46 +76,61 @@ export function Register() {
   }
 
   return (
-    <Container>
-      <Header>
-        <Title>Cadastro</Title>
-      </Header>
-      <Form>
-        <Fields>
-          <InputForm name="name" control={control} placeholder="Nome" />
-          <InputForm name="amount" control={control} placeholder="Preço" />
-
-          <TransactionsTypes>
-            <TransactionTypeButton
-              isActive={transactionType === "up"}
-              title="Income"
-              type="up"
-              onPress={() => handleTransactionTypeSelect("up")}
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <Container>
+        <Header>
+          <Title>Cadastro</Title>
+        </Header>
+        <Form>
+          <Fields>
+            <InputForm
+              name="name"
+              control={control}
+              placeholder="Nome"
+              autoCapitalize="sentences"
+              autoCorrect={false}
+              error={errors.name && errors.name.message}
             />
-            <TransactionTypeButton
-              isActive={transactionType === "down"}
-              title="Outcome"
-              type="down"
-              onPress={() => handleTransactionTypeSelect("down")}
+            <InputForm
+              name="amount"
+              control={control}
+              placeholder="Preço"
+              keyboardType="numeric"
+              error={errors.name && errors.amount.message}
             />
-          </TransactionsTypes>
 
-          <CategorySelectButton
-            title={category.name}
-            onPress={handleOpenSelectCategoryModal}
+            <TransactionsTypes>
+              <TransactionTypeButton
+                isActive={transactionType === "up"}
+                title="Income"
+                type="up"
+                onPress={() => handleTransactionTypeSelect("up")}
+              />
+              <TransactionTypeButton
+                isActive={transactionType === "down"}
+                title="Outcome"
+                type="down"
+                onPress={() => handleTransactionTypeSelect("down")}
+              />
+            </TransactionsTypes>
+
+            <CategorySelectButton
+              title={category.name}
+              onPress={handleOpenSelectCategoryModal}
+            />
+          </Fields>
+
+          <Button title="Enviar" onPress={handleSubmit(handleRegister)} />
+        </Form>
+
+        <Modal visible={categoryModalOpen}>
+          <CategorySelect
+            category={category}
+            setCategory={setCategory}
+            closeSelectCategory={handleCloseSelectCategoryModal}
           />
-        </Fields>
-
-        <Button title="Enviar" onPress={handleSubmit(handleRegister)} />
-      </Form>
-
-      <Modal visible={categoryModalOpen}>
-        <CategorySelect
-          category={category}
-          setCategory={setCategory}
-          closeSelectCategory={handleCloseSelectCategoryModal}
-        />
-      </Modal>
-    </Container>
+        </Modal>
+      </Container>
+    </TouchableWithoutFeedback>
   );
 }
