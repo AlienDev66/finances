87b84306b13f35a95fe4210344de8057442b061a
Domingos Capitango <p.capitango65@gmail.com>
2022-01-02 21:45:17 +0100

    :sparkles: add resume screen.

---
+++ src/screens/Resume/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { Text } from "react-native";
+
+import { Container } from "./styles";
+
+export function Resume() {
+  return (
+    <Container>
+      <Text>Resume</Text>
+    </Container>
+  );
+}
