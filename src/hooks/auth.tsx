af0bdb76c6491d6e9c9daa12cb5a2420c9145ef3
Domingos Capitango <p.capitango65@gmail.com>
2022-01-03 02:00:41 +0100

    :sparkles: :config: add login screen and configure context.

---
+++ src/hooks/auth.tsx
@@ -0,0 +1,38 @@
+import React, { createContext, ReactNode, useContext } from "react";
+
+interface AuthProviderProps {
+  children: ReactNode;
+}
+
+interface User {
+  id: string;
+  name: string;
+  email: string;
+  photo?: string;
+}
+
+interface AuthContextData {
+  user: User;
+}
+
+const AuthContext = createContext({} as AuthContextData);
+
+function AuthProvider({ children }: AuthProviderProps) {
+  const user = {
+    id: "djhakdjskflas",
+    name: "Domingos Capitango",
+    email: "domingosmanuelcapitango@gmail.com",
+    photo: "dskhjakdsfdkslf",
+  };
+  return (
+    <AuthContext.Provider value={{ user }}>{children}</AuthContext.Provider>
+  );
+}
+
+function useAuth() {
+  const context = useContext(AuthContext);
+
+  return context;
+}
+
+export { AuthProvider, useAuth };
